c90a24902b3dffb4321cce3219c03ddb04862786
jide solanke <45001634+vistait2018@users.noreply.github.com>
2022-02-11 17:21:44 +0100

    CSRF-TOKEN

--- src/main/java/com/example/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.example.security.ApplicationUserRole.*;
 
@@ -31,6 +32,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
        http
                .csrf().disable()
+//               .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//               .and()
                .authorizeRequests()
                .antMatchers("/","index","/css/*","/js/*").permitAll()
                .antMatchers("/api/**").hasRole(STUDENT.name())
